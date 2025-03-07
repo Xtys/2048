462f98d308a49bd2fb315682f411d8170006a76c
condedan0 <styxmodz@gmail.com>
2025-03-07 16:27:44 +1000

    Add files via upload

---
+++ comp2048_a1/test_caesar.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Caesar cypher script
+
+Encode and decode messages by scrambling the letters in your message
+
+Created on Fri Feb  1 23:06:50 2019
+
+@author: shakes
+"""
+import string
+
+letters = string.ascii_letters #contains 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+message = "WHERE is Alfred our homie cyclone is he trolling us" #type your message here
+print("Message:", message)
+
+#create the Caesar cypher
+offset = 10 #choose your shift
+totalLetters = 26
+keys = {} #use dictionary for letter mapping
+invkeys = {} #use dictionary for inverse letter mapping, you could use inverse search from original dict
+for index, letter in enumerate(letters):
+    # cypher setup
+    if index < totalLetters: #lowercase
+        #INSERT CODE HERE
+        shift_letter = chr((ord(letter) + offset - 97 )% totalLetters + 97)
+        keys[letter] = shift_letter
+    else: #uppercase
+        #INSERT CODE HERE
+       shift_letter = chr((ord(letter)  + offset - 65) % 26 + 65)
+       keys[letter] = shift_letter
+
+invkeys = {v: k for k, v in keys.items()}
+
+print("Cypher Dict:", keys)
+
+#encrypt
+encryptedMessage = []
+for letter in message:
+    if letter == ' ': #spaces
+        encryptedMessage.append(letter)
+    else:
+        encryptedMessage.append(keys[letter])
+print("Encrypted Message:", ''.join(encryptedMessage)) #join is used to put list inot string
+
+#decrypt
+decryptedMessage = []
+for letter in encryptedMessage:
+    if letter == ' ': #spaces
+        decryptedMessage.append(letter)
+    else:
+        decryptedMessage.append(invkeys[letter])
+print("Decrypted Message:", ''.join(decryptedMessage)) #join is used to put list inot string
