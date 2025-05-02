8475985e76d4dab40989b46cab5be32d81f3abcc
condedan0 <styxmodz@gmail.com>
2025-05-02 16:43:31 +1000

    Add files via upload
    test

--- comp2048_a1/test_caesar.py
+++ comp2048_a1/test_caesar.py
@@ -12,7 +12,7 @@ import string
 
 letters = string.ascii_letters #contains 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-message = "let use Try another one" #type your message here
+message = "Hi I am brandon and I am a com sci student" #type your message here
 print("Message:", message)
 
 #create the Caesar cypher
@@ -24,6 +24,8 @@ for index, letter in enumerate(letters):
     # cypher setup
     if index < totalLetters: #lowercase
         #INSERT CODE HERE
+        #letters to ASCII 97 - 122, value then normalize the range to 0–25
+        # wrap around the alphabet if the shift exceeds 'z' : integers' congruence property
         shift_letter = chr((ord(letter) + offset - 97 )% totalLetters + 97)
         keys[letter] = shift_letter
     else: #uppercase
@@ -31,6 +33,8 @@ for index, letter in enumerate(letters):
        shift_letter = chr((ord(letter)  + offset - 65) % 26 + 65)
        keys[letter] = shift_letter
 
+#creates the inverse mapping of the keys
+# reverse lookup v: k for k,
 invkeys = {v: k for k, v in keys.items()}
 
 print("Cypher Dict:", keys)
--- comp2048_a1/test_caesar_break.py
+++ comp2048_a1/test_caesar_break.py
@@ -9,7 +9,11 @@ Created on Sat Feb  2 23:03:02 2019
 from collections import Counter
 import string
 
-message =  "Zyp cpxpxmpc ez wzzv fa le esp delcd lyo yze ozhy le jzfc qppe Ehz ypgpc rtgp fa hzcv Hzcv rtgpd jzf xplytyr lyo afcazdp lyo wtqp td pxaej hteszfe te Escpp tq jzf lcp wfnvj pyzfrs ez qtyo wzgp cpxpxmpc te td espcp lyo ozye esczh te lhlj Depaspy Slhvtyr"
+message =  "Pbqf xob qeb ybpq qefkdp fk qeb tloia"
+#"Zyp cpxpxmpc ez wzzv fa le esp delcd lyo yze ozhy le jzfc qppe Ehz ypgpc rtgp fa hzcv Hzcv rtgpd jzf xplytyr lyo afcazdp lyo wtqp td pxaej hteszfe te Escpp tq jzf lcp wfnvj pyzfrs ez qtyo wzgp cpxpxmpc te td espcp lyo ozye esczh te lhlj Depaspy Slhvtyr"
+# "Qefp fp x obxi tloia tfqe dlob xka xq qeb pxjb qfjb qexq f exsb ybba qlooba qeb obxi tloia fp qeb obxi tloia"
+# "Pbqf xob qeb ybpq qefkdp fk qeb tloia"
+# "Khoor zruog"
 
 #frequency of each letter
 letter_counts = Counter(message)
@@ -21,6 +25,7 @@ maxLetter = None
 for letter, freq in letter_counts.items():
     print(letter, ":", freq)
     #INSERT CODE TO REMEMBER MAX
+    # idea here is, if empty skip, if freq bigger than maxFreq, set
     if letter != " ":
         if freq > maxFreq:
             maxFreq = freq
@@ -28,16 +33,18 @@ for letter, freq in letter_counts.items():
 print("Max Ocurring Letter:", maxLetter)
 
 #predict shift
-#assume max letter is 'e'
 letters = string.ascii_letters #contains 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-# shift = ((ord(maxLetter) - 97) - ord('e') - 97) % 26
+#assume max letter is 'e'
+def shifter(maxLetter):
+    shift = (ord(maxLetter) - 101) % 26
+    print("Predicted Shift:", shift)
+    return shift
 
-#part c
-shift = (ord(maxLetter) - 101) % 26
-print("Predicted Shift:", shift)
+shift = shifter(maxLetter)
 
 totalLetters = 26
 keys = {}
+#part c, only work well with words have 'e'
 for index, letter in enumerate(letters):
     if index < totalLetters:  # lowercase
         og_letter = chr((ord(letter) - 97 - shift) % 26 + 97)
